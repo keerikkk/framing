68c386d33f13ffbc67d0b086265fd51f55aa5347
Kyrylo Svir <ksvir17@gmail.com>
2024-09-13 15:29:20 +0300

    feat: add testimonial-header

---
+++ src/components/TestimonialList/Testimonial/TestimonialHeader.tsx
@@ -0,0 +1,27 @@
+export interface TestimonialHeaderProps {
+  img: string;
+  author: string;
+  platform: string;
+}
+
+const TestimonialHeader = ({
+  img,
+  author,
+  platform,
+}: TestimonialHeaderProps) => {
+  return (
+    <div className='flex gap-8'>
+      <img
+        src={img}
+        alt={author}
+        className='w-16 aspect-square object-cover rounded-full'
+      />
+      <div className='flex flex-col text-left'>
+        <h2 className='font-cormorant font-semibold text-[1.5rem]'>{author}</h2>
+        <p>{platform}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialHeader;
