d57aac3c40b9fd0d970765ad25f02072f44b1f0d
Kyrylo Svir <ksvir17@gmail.com>
2024-09-13 20:38:31 +0300

    style: add responsiveness

--- src/components/TestimonialList/Testimonial/TestimonialHeader.tsx
+++ src/components/TestimonialList/Testimonial/TestimonialHeader.tsx
@@ -17,7 +17,9 @@ const TestimonialHeader = ({
         className='w-16 aspect-square object-cover rounded-full'
       />
       <div className='flex flex-col text-left'>
-        <h2 className='font-cormorant font-semibold text-[1.5rem]'>{author}</h2>
+        <h2 className='font-cormorant font-semibold text-[1.5rem] whitespace-nowrap overflow-hidden text-ellipsis'>
+          {author}
+        </h2>
         <p>{platform}</p>
       </div>
     </div>
--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -4,7 +4,9 @@ interface HeaderProps {
 
 const Header = ({ title }: HeaderProps) => {
   return (
-    <h1 className='font-cormorant text-6xl text-white text-left'>{title}</h1>
+    <h1 className='font-cormorant text-4xl sm:text-5xl lg:text-6xl text-white text-left'>
+      {title}
+    </h1>
   );
 };
 
