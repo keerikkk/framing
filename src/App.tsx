f97380e6d5886b477d22aff823f7e5cdfffa599e
Kyrylo Svir <ksvir17@gmail.com>
2024-09-13 15:30:29 +0300

    feat: add testimonials-page to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import BenefitsPage from './pages/BenefitsPage/BenefitsPage';
 import PriceListPage from './pages/PriceListPage';
+import TestimonialsPage from './pages/TestimonialsPage';
 
 function App() {
   return (
     <div>
       <BenefitsPage />
+      <TestimonialsPage />
       <PriceListPage />
     </div>
   );
