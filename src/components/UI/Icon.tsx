597c5f2820b2a1f95711d524782a3f691ec5b167
Kyrylo Svir <ksvir17@gmail.com>
2024-09-14 15:46:29 +0300

    feat: add hover-name option for icon

--- src/components/UI/Icon.tsx
+++ src/components/UI/Icon.tsx
@@ -3,14 +3,32 @@ import sprite from '..//../../public/sprite.svg';
 
 interface IconProps {
   name: string;
+  hoverName?: string;
   className?: string;
 }
 
-const Icon = ({ name, className }: IconProps) => {
+const Icon = ({ name, hoverName, className }: IconProps) => {
   return (
-    <svg className={cn('text-basic h-7 w-7', className)}>
-      <use href={`${sprite}#${name}`} />
-    </svg>
+    <div className={`relative inline-block ${hoverName ? 'group' : ''}`}>
+      <svg
+        className={cn(
+          `text-basic h-7 w-7 block transition-opacity duration-300 ${hoverName ? 'opacity-100 group-hover:opacity-0' : ''}`,
+          className,
+        )}
+      >
+        <use href={`${sprite}#${name}`} />
+      </svg>
+      {hoverName && (
+        <svg
+          className={cn(
+            'text-basic h-7 w-7 absolute top-0 left-0 transition-opacity duration-300 opacity-0 group-hover:opacity-100',
+            className,
+          )}
+        >
+          <use href={`${sprite}#${hoverName}`} />
+        </svg>
+      )}
+    </div>
   );
 };
 
