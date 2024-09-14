ac19cc5c00a3d6468a1398c2897d68042fa2dd61
Kyrylo Svir <ksvir17@gmail.com>
2024-09-14 15:48:13 +0300

    feat: add home-page to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import BenefitsPage from './pages/BenefitsPage/BenefitsPage';
+import HomePage from './pages/HomePage';
 import PriceListPage from './pages/PriceListPage';
 import TestimonialsPage from './pages/TestimonialsPage';
 
 function App() {
   return (
     <div>
+      <HomePage />
       <BenefitsPage />
       <TestimonialsPage />
       <PriceListPage />
