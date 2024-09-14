c053cd1d1fbee231012deda13cca2ed805e05294
Kyrylo Svir <ksvir17@gmail.com>
2024-09-14 15:47:25 +0300

    feat: add home-description

---
+++ src/components/HomeDescription.tsx
@@ -0,0 +1,14 @@
+const HomeDescription = () => {
+  return (
+    <div className='absolute bottom-1/4 translate-y-1/4 right-1/3 translate-x-1/3 md:right-[10%] md:-translate-x-[10%] max-w-xs'>
+      <p className='text-basic text-lg md:text-xl text-left'>{data.content}</p>
+    </div>
+  );
+};
+
+export default HomeDescription;
+
+const data = {
+  content:
+    'Pellentesque mauris a lobortis in bibendum sed lobortis semper. Eget eu vel eu vitae.',
+};
