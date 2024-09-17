52300afd48b8e497bc9d0def1fe8089d5d16808c
Kyrylo Svir <ksvir17@gmail.com>
2024-09-17 20:55:42 +0300

    feat: add mobile version of benefit-list

--- src/pages/BenefitsPage/BenefitList.tsx
+++ src/pages/BenefitsPage/BenefitList.tsx
@@ -1,8 +1,21 @@
-import Benefit from '@/components/Benefit';
+import Benefit, { BenefitProps } from '@/components/Benefit';
+import Icon from '@/components/UI/Icon';
+import { useState } from 'react';
 
 const BenefitList = () => {
   return (
-    <div className='contents max-sm:flex max-sm:flex-col max-sm:items-center sm:items-stretch'>
+    <>
+      <BenefitListDesktop />
+      <BenefitListMobile />
+    </>
+  );
+};
+
+export default BenefitList;
+
+const BenefitListDesktop = () => {
+  return (
+    <div className='sm:items-stretch hidden sm:contents'>
       {data.map((benefit) => (
         <Benefit key={benefit.title} {...benefit} />
       ))}
@@ -10,9 +23,43 @@ const BenefitList = () => {
   );
 };
 
-export default BenefitList;
+const BenefitListMobile = () => {
+  const [selectedBenefit, setSelectedBenefit] = useState(data[0]);
 
-const data = [
+  const handleBenefitClick = (benefit: BenefitProps) => {
+    setSelectedBenefit(benefit);
+  };
+
+  return (
+    <div className='w-full mx-auto px-4 pt-8 block sm:hidden'>
+      <div className='flex justify-around mb-4'>
+        {data.map((benefit) => {
+          return (
+            <button
+              key={benefit.title}
+              onClick={() => handleBenefitClick(benefit)}
+              className={`flex flex-col items-center p-1  transition-colors bg-transparent ${
+                selectedBenefit === benefit
+                  ? 'border-b border-teal-500 shadow-md shadow-teal-bright'
+                  : ''
+              }`}
+              aria-expanded={selectedBenefit === benefit}
+              aria-controls={`benefit-${benefit.title}`}
+            >
+              <Icon
+                name={benefit.icon}
+                className={`${selectedBenefit === benefit && 'text-white'}`}
+              />
+            </button>
+          );
+        })}
+      </div>
+      {selectedBenefit && <Benefit {...selectedBenefit} />}
+    </div>
+  );
+};
+
+const data: BenefitProps[] = [
   {
     icon: 'agency',
     title: 'Agency',
