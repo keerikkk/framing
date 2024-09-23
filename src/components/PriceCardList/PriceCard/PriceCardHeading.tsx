267577316ddff24a3ae30d03b09ca4a94a5228a1
Kyrylo Svir <ksvir17@gmail.com>
2024-09-23 10:22:47 +0300

    style: change text alignment

--- src/components/PriceCardList/PriceCard/PriceCardHeading.tsx
+++ src/components/PriceCardList/PriceCard/PriceCardHeading.tsx
@@ -10,7 +10,7 @@ const PriceCardHeading = ({
   currency,
 }: PriceCardHeadingProps) => {
   return (
-    <div className='flex flex-col gap-1.5 text-white'>
+    <div className='flex flex-col gap-1.5 text-white text-center'>
       <h3 className='text-2xl font-medium'>{title}</h3>
       <span className='text-4xl font-normal font-cormorant'>
         {value}
