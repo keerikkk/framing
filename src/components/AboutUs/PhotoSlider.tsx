082bfb84133a117d23737540afa6aa88b4b4e6aa
Kyrylo Svir <ksvir17@gmail.com>
2024-09-23 14:45:29 +0300

    feat: add pocket-base for back-end & fetch photos

--- src/components/AboutUs/PhotoSlider.tsx
+++ src/components/AboutUs/PhotoSlider.tsx
@@ -1,9 +1,14 @@
+import { FC } from 'react';
 import { Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
 
-const PhotoSlider = () => {
+interface PhotoSliderProps {
+  photos: any[];
+}
+
+const PhotoSlider: FC<PhotoSliderProps> = ({ photos }) => {
   return (
     <article className='pt-4 md:pt-14 lg:pt-20'>
       <Swiper
@@ -27,14 +32,14 @@ const PhotoSlider = () => {
           },
         }}
       >
-        {data.map((image) => (
+        {photos.map((photo) => (
           <SwiperSlide
-            key={image.src}
+            key={photo.id}
             className='max-w-36 md:max-w-48 lg:max-w-60'
           >
             <img
-              src={image.src}
-              alt={image.alt}
+              src={photo.image}
+              alt='Slider Image'
               className='aspect-[293/358] object-cover'
             />
           </SwiperSlide>
@@ -45,46 +50,3 @@ const PhotoSlider = () => {
 };
 
 export default PhotoSlider;
-
-const data = [
-  {
-    src: 'https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?cs=srgb&dl=pexels-soldiervip-1308881.jpg&fm=jpg',
-    alt: 'Image 1',
-  },
-  {
-    src: 'https://images.pexels.com/photos/35537/child-children-girl-happy.jpg?cs=srgb&dl=pexels-bess-hamiti-83687-35537.jpg&fm=jpg',
-    alt: 'Image 2',
-  },
-  {
-    src: 'https://static.vecteezy.com/system/resources/thumbnails/026/829/465/small/beautiful-girl-with-autumn-leaves-photo.jpg',
-    alt: 'Image 3',
-  },
-  {
-    src: 'https://www.freedigitalphotos.net/images/img/homepage/339504.jpg',
-    alt: 'Image 4',
-  },
-  {
-    src: 'https://images.squarespace-cdn.com/content/v1/58d1b3ff1b631bb1893d108d/813f4928-6cc6-4bc8-a4e4-265f94b4d665/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg',
-    alt: 'Image 5',
-  },
-  {
-    src: 'https://images.ctfassets.net/hrltx12pl8hq/3Mz6t2p2yHYqZcIM0ic9E2/3b7037fe8871187415500fb9202608f7/Man-Stock-Photos.jpg',
-    alt: 'Image 6',
-  },
-  {
-    src: 'https://photoscissors.com/images/samples/1-before.jpg',
-    alt: 'Image 7',
-  },
-  {
-    src: 'https://images.ctfassets.net/hrltx12pl8hq/12wPNuS1sirO3hOes6l7Ds/9c69a51705b4a3421d65d6403ec815b1/non_cheesy_stock_photos_cover-edit.jpg',
-    alt: 'Image 8',
-  },
-  {
-    src: 'https://cdn0.weddingwire.in/article/2542/3_2/960/jpg/112452-engagement-photos-closeup-majestic-photography.jpeg',
-    alt: 'Image 9',
-  },
-  {
-    src: 'https://people.com/thmb/W9hu212ZarEmX3eMRWsS1XrjjPw=/4000x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/michael-caine69-987b236327764608ad200351f28e0881.jpg',
-    alt: 'Image 10',
-  },
-];
--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,17 +1,25 @@
-import AboutUsTitle from '@/components/AboutUs/AbouUsTitle';
+import PocketBase from 'pocketbase';
+
 import ExperienceList from '@/components/AboutUs/ExperienceList';
 import PhotoSlider from '@/components/AboutUs/PhotoSlider';
+import AboutUsTitle from '@/components/AboutUs/AbouUsTitle';
 
-const AbousUsPage = () => {
+async function getPhotos() {
+  const db = new PocketBase('http://127.0.0.1:8090');
+  const data = await db.collection('pictures').getList();
+  return data.items;
+}
+
+export default async function AboutUsPage() {
+  const photos = await getPhotos();
+
   return (
-    <section id='#contact' className=' bg-benefits'>
+    <section id='contact' className='bg-benefits'>
       <div className='container mx-auto flex flex-col gap-20 py-24 px-2'>
         <AboutUsTitle />
         <ExperienceList />
-        <PhotoSlider />
+        <PhotoSlider photos={photos} />
       </div>
     </section>
   );
-};
-
-export default AbousUsPage;
+}
