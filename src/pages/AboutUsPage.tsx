98080ca6ba4bdc97aad175972065586d5cf6b6f9
Kyrylo Svir <ksvir17@gmail.com>
2024-09-25 18:40:36 +0300

    fix: fix issue with server-side & client-side components

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,24 +1,14 @@
-import PocketBase from 'pocketbase';
-
 import ExperienceList from '@/components/AboutUs/ExperienceList';
 import PhotoSlider from '@/components/AboutUs/PhotoSlider';
 import AboutUsTitle from '@/components/AboutUs/AbouUsTitle';
 
-async function getPhotos() {
-  const db = new PocketBase('http://127.0.0.1:8090');
-  const data = await db.collection('pictures').getList();
-  return data.items;
-}
-
-export default async function AboutUsPage() {
-  const photos = await getPhotos();
-
+export default function AboutUsPage() {
   return (
     <section id='contact' className='bg-benefits'>
       <div className='container mx-auto flex flex-col gap-20 py-24 px-2'>
         <AboutUsTitle />
         <ExperienceList />
-        <PhotoSlider photos={photos} />
+        <PhotoSlider />
       </div>
     </section>
   );
