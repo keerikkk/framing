9138fe791cd20f95e1589d3dd7a5da0e0e16c065
Kyrylo Svir <ksvir17@gmail.com>
2024-09-27 20:59:24 +0300

    feat: add smooth scroll to sections

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import HomeDescription from '@/components/Home/HomeDescription';
 const HomePage = () => {
   return (
     <section
-      id='home'
+      id='#home'
       className='relative bg-teal-light flex flex-col min-h-screen before:absolute before:bg-benefits before:top-0 before:right-0 before:w-1/2 before:h-full before:content-[""]'
     >
       <div className='container mx-auto flex flex-col gap-20 py-48 px-2 relative flex-1'>
