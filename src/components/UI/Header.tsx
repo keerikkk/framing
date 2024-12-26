88491385489ff84ae4d9d9692bbfbf0c6e638610
Kyrylo Svir <ksvir17@gmail.com>
2024-12-26 18:22:16 +0200

    style: change z-index for header

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ title, className }: HeaderProps) => {
   return (
     <h1
       className={cn(
-        'text-left font-cormorant text-4xl text-blue-dark dark:text-white sm:text-5xl lg:text-6xl',
+        'relative z-10 text-left font-cormorant text-4xl text-blue-dark dark:text-white sm:text-5xl lg:text-6xl',
         className,
       )}
     >
