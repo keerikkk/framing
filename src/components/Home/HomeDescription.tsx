07692a89d82f0e840a176f95a1561e80beae1ae6
Kyrylo Svir <ksvir17@gmail.com>
2024-12-27 19:49:24 +0200

    feat: add animation for home page

--- src/components/Home/HomeDescription.tsx
+++ src/components/Home/HomeDescription.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
 const HomeDescription = () => {
   return (
     <div className='absolute bottom-1/4 right-1/3 max-w-xs translate-x-1/3 translate-y-1/4 md:right-[10%] md:-translate-x-[10%]'>
-      <p className='text-left text-lg text-paragraph-dark dark:text-gray-400 md:text-xl'>
+      <motion.p
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ amount: 0.5, once: true }}
+        transition={{ duration: 2 }}
+        className='text-left text-lg text-paragraph-dark dark:text-gray-400 md:text-xl'
+      >
         {data.content}
-      </p>
+      </motion.p>
     </div>
   );
 };
