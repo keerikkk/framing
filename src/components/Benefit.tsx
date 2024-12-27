f5512ba4639d5be8eb3f6a96b2c3a58b2fa29c1e
Kyrylo Svir <ksvir17@gmail.com>
2024-12-27 23:26:44 +0200

    feat: add animation for benefits page

--- src/components/Benefit.tsx
+++ src/components/Benefit.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
 import Icon from './UI/Icon';
 
 export interface BenefitProps {
@@ -6,9 +10,26 @@ export interface BenefitProps {
   text: string;
 }
 
+const benefits = {
+  hidden: {
+    opacity: 0,
+    x: 10,
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
 const Benefit = ({ icon, title, text }: BenefitProps) => {
   return (
-    <article className='group max-w-max border-transparent transition-all duration-300 hover:md:border-[.375rem] hover:md:border-cream-light hover:md:border-opacity-50 hover:md:shadow-md hover:md:shadow-cream-light dark:hover:md:border-cyan-950 dark:hover:md:shadow-cyan-950'>
+    <motion.article
+      variants={benefits}
+      className='group max-w-max border-transparent transition-all duration-300 hover:md:border-[.375rem] hover:md:border-cream-light hover:md:border-opacity-50 hover:md:shadow-md hover:md:shadow-cream-light dark:hover:md:border-cyan-950 dark:hover:md:shadow-cyan-950'
+    >
       <div className='my-6 ms-6 flex max-w-56 flex-col gap-2 text-left text-paragraph-dark transition-all duration-300 group-hover:text-cream-dark dark:text-gray-400 dark:group-hover:text-white lg:me-20'>
         <Icon
           name={icon}
@@ -17,7 +38,7 @@ const Benefit = ({ icon, title, text }: BenefitProps) => {
         <h2>{title}</h2>
         <p>{text}</p>
       </div>
-    </article>
+    </motion.article>
   );
 };
 
--- src/pages/BenefitsPage/BenefitsPage.tsx
+++ src/pages/BenefitsPage/BenefitsPage.tsx
@@ -1,13 +1,33 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
 import BenefitsPageHeader from './BenefitsHeader/BenefitsPageHeader';
 import BenefitList from './BenefitList';
+
+const variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
 
 const BenefitsPage = () => {
   return (
     <section id='#benefits' className='bg-blue-light dark:bg-black-light'>
-      <div className='container mx-auto flex flex-col gap-1 px-2 py-24 sm:grid sm:grid-cols-2 md:grid-cols-3 xl:gap-4'>
+      <motion.div
+        variants={variants}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ amount: 0.5, once: true }}
+        className='container mx-auto flex flex-col gap-1 px-2 py-24 sm:grid sm:grid-cols-2 md:grid-cols-3 xl:gap-4'
+      >
         <BenefitsPageHeader />
         <BenefitList />
-      </div>
+      </motion.div>
     </section>
   );
 };
