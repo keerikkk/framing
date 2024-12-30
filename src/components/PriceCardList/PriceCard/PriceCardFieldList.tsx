130f3a66fd992b0f18fecd66e4eaf2408244592c
Kyrylo Svir <ksvir17@gmail.com>
2024-12-30 18:23:31 +0200

    refactor: change pricing info

--- src/components/PriceCardList/PriceCard/PriceCardFieldList.tsx
+++ src/components/PriceCardList/PriceCard/PriceCardFieldList.tsx
@@ -6,7 +6,7 @@ interface PriceCardFieldListProps {
 
 const PriceCardFieldList = ({ fields }: PriceCardFieldListProps) => {
   return (
-    <ul className='flex flex-col gap-6 py-6'>
+    <ul className='flex grow flex-col justify-evenly gap-6 py-6'>
       {fields &&
         fields.map((field) => <PriceCardField key={field.label} {...field} />)}
     </ul>
