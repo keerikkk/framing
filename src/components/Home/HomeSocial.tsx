6e54991134a266dc4423aca59aaecb02d202c83b
Kyrylo Svir <ksvir17@gmail.com>
2024-12-30 18:57:36 +0200

    refactor: change home content

--- src/components/Home/HomeSocial.tsx
+++ src/components/Home/HomeSocial.tsx
@@ -6,9 +6,8 @@ const HomeSocial = () => {
     <div className='absolute right-0 top-1/2 h-3/4 w-7 -translate-y-1/2 border-l-2 border-cream-bright dark:border-teal-bright'>
       <div className='home-social'>
         {socialData.map((icon) => (
-          <Link href={icon.to} target='_blank'>
+          <Link key={icon.name} href={icon.to} target='_blank'>
             <Icon
-              key={icon.name}
               {...icon}
               className='text-cream-bright dark:text-teal-bright'
             />
@@ -25,7 +24,7 @@ export const socialData = [
   {
     name: 'google',
     hoverName: 'google-colored',
-    to: 'mailto:framing.studio@gmail.com',
+    to: 'mailto:framing.studio.uz@gmail.com',
   },
   {
     name: 'instagram',
